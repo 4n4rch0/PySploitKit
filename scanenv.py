f6d967b99036c4f318bec6197c6212733186dead
4N4RCH0 <anarchodeveloper@gmail.com>
2024-07-14 03:33:48 +0200

    added main script and optimized scanner lib

---
+++ scanenv.py
@@ -0,0 +1,20 @@
+import scapy.all as scapy
+
+class LocalNetwork:
+
+    def __init__(self):
+        pass
+
+    def arp_scan(self, ip_network_data):
+        pass
+
+    def ping_scan(self, ip_network_data):
+        pass
+
+class Internet:
+
+    def __init__(self) -> None:
+        pass
+
+    def port_scan():
+        pass
