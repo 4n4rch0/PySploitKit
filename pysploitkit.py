0a1256b2b4919ff5fb7f0569496db9540fc15bb3
4N4RCH0 <anarchodeveloper@gmail.com>
2024-07-28 04:38:26 +0200

    commit

--- pysploitkit.py
+++ pysploitkit.py
@@ -6,28 +6,8 @@ def banner(titel):
     f = Figlet(font='slant')
     print(f.renderText(titel))
 
-<<<<<<< HEAD
 class Coordination:
     pass
-=======
-class WebRequest:
-
-    def __init__(self) -> None:
-        pass
-
-    def icmp_reply(self, ip_address):
-
-        ping_process = subprocess.Popen(['ping', '-c', '4', ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        ping_output, ping_errors = ping_process.communicate()
-
-        print("[ICMP ECHO REPLY]" f'{ping_output}')
-        print("[ICMP RESPONSE]" f'{ping_errors}')
-
-class Discovery:
-
-    def __init__(self):
-        pass
->>>>>>> a192c72896e330fb44149e144d5578b06e8685c8
 
 def main():
 
