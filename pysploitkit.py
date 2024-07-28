3e84180f78041567a1a1b8d1814fe8ca27c00785
4N4RCH0 <anarchodeveloper@gmail.com>
2024-07-28 07:01:07 +0200

    updated code struct

--- pysploitkit.py
+++ pysploitkit.py
@@ -1,16 +1,35 @@
 import sys
+import os
 import scanenv
 from pyfiglet import Figlet
 
+os.system("clear")
 
 def banner(titel):
     f = Figlet(font='avatar')
     print(f.renderText(titel))
 
+# contains any active recon scan option
+class Hostdiscovery:
+
+    def __init__(self, discover_command):
+        self.discover_command = discover_command
+
+    def ping_host(ip_address):
+        scanenv.icmp_echo(ip_address)
+
+
+# contains any control option of the user and its mechanisms
 class AttackController:
 
-    def __init__(self, user_action):
-        self.user_action = user_action
+    def __init__(self, user_command):
+        self.user_command = user_command
+
+    def user_action(user_command):
+        if user_command == "1":
+            ip_destionation = input("[*] DESTINATION IP ADDRESS: ")
+            scanenv.icmp_echo(ip_destionation)
+
 
 def main():
 
@@ -18,8 +37,12 @@ def main():
 
         banner("PY$SPLOIT")
 
-        command = input("[1]\tICMP ECHO")
-        AttackController(command)
+        print("[1]\tICMP ECHO REPLY (ping)")
+        print("[99]\tEXIT\n")
+
+        user_command = input("[$]> ")
+
+        AttackController(user_command).user_action()
 
 
     except KeyboardInterrupt:
--- scanenv.py
+++ scanenv.py
@@ -1,15 +1,9 @@
 import time
 import os
 
-class RequestHost:
-
-    def __init__(self):
-        pass
-
 def icmp_echo(ip_destionation):
     response = os.system("ping -c 4 " + ip_destionation)
     if response == 0:
         print("PING {}.".format(response))
-        time.sleep()
     else:
         pass
