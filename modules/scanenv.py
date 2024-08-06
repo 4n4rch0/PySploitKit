cc006159551043003b9a6f3b8a290c7058a00026
4N4RCH0 <anarchodeveloper@gmail.com>
2024-08-07 00:43:53 +0200

    optimized nmap scanning

--- modules/scanenv.py
+++ modules/scanenv.py
@@ -44,7 +44,7 @@ def icmp_host_discover(network_range):
 
 def nmap_host_discover(network_range):
     nm = nmap.PortScanner()
-    nm.scan(hosts=network_range, arguments='-sn -A')
+    nm.scan(hosts=network_range, arguments='-sR -Pn')
 
     live_hosts = []
     for host in nm.all_hosts():
